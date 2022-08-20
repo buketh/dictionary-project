7db0c3299f8e367bb85ed494ca53e8b0e62ad626
buketh <buket.hepgunay@gmail.com>
2022-08-20 11:28:50 +0200

    Created form and defined searchEvent

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import logo from "./logo.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo"></img>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo"></img>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer"> Coded by SheCodes</footer>
+      </div>
     </div>
   );
 }
